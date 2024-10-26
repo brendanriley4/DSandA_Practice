8b24db8cd19591b857d0fe99fb636355310a376b
Brendan Riley <Brendan.C.Riley@dynetics.com>
2024-10-26 18:49:11 -0500

    I decided to do a couple more this afternoon.

--- Easy/twoSum.py
+++ Easy/twoSum.py
@@ -5,3 +5,11 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+
+        myDict = {nums[0]: 0}
+
+        for i in range(1, len(nums)):
+            if target - nums[i] in myDict:
+                return [i, myDict[target - nums[i]]]
+            myDict[nums[i]] = i
+        return []
